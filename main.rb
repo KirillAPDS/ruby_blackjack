b3923da1764486ef66acfd7ca0c525a28845b719
Kirill <massage.kir@gmail.com>
2024-06-13 12:27:30 +0300

    added content to round  method

--- main.rb
+++ main.rb
@@ -7,16 +7,16 @@ class Main
   attr_accessor :bank
 
   MENU = [
-    '1 - skip',
-    '2 - add a card',
-    '3 - open cards'
+    '1 - пропустить',
+    '2 - добавить карту',
+    '3 - открыть карты'
   ]
 
   def initialize
-    puts 'Input your name'
+    puts 'Введите ваше имя'
     @name = gets.chomp
     @user = Player.new(@name)
-    @dealer = Player.new('Dealer')
+    @dealer = Player.new('Дилер')
     @deck = Deck.new
     @bank = 0
   end
@@ -29,7 +29,35 @@ class Main
     # раздача карт игрокам
     take_card(user, 2)
     take_card(dealer, 2)
+    # игровой цикл с выбором из MENU
+    loop do
+      # показываем карты игроков
+      user.show_cards(user.name)
+      dealer.show_cards(dealer.name, false)
+      # выбор действия
+      puts MENU
+      choice = gets.chomp.to_i
+      case choice
+      when 1 then dealer_move # пропускаем ход, ход дилера
+      when 2 # добавляем карту себе и дилеру
+        take_card(user) # if user.cards.size < 3
+        dealer_move
+      when 3 # открываем карты. дилер при этом может сделать ход
+        dealer_move
+        score(user) # показываем счет 
+        score(dealer)
+      end
+      
+    end
+    # определяем победителя и обнуляем карты игроков
+    define_winner
+    user.reset
+    dealer.reset
     # todo
+  end
+
+  def score(player)
+    puts "#{player.name}: #{player.score} очков"
   end
 
   def take_card(player, count = 1)
