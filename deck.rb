7056f0e24b20bdace8c9cb01d53c12698f56987c
Kirill <massage.kir@gmail.com>
2024-06-14 12:25:22 +0300

    changed VALUES const

--- deck.rb
+++ deck.rb
@@ -2,7 +2,8 @@ class Deck
   attr_accessor :cards
 
   SUITS = %w[♥ ♦ ♣ ♠]
-  VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+  # VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 
   def initialize
     @cards = []
